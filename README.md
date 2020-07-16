855d3b936f1c61140487ea46a0d70787c37d2b0e
acierto <bogdannechiporenko@gmail.com>
2020-07-16 10:23:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Git Branch Remover
+
+Specific plugin written exclusively for XebiaLabs products to remove outdated release branches.
+ 
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import {exec} from 'child_process';
+
+const getCurrentDayOfYear = () => {
+    const now: any = new Date();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    return getDaysInMonth(month) * month + day;
+};
+
+const getDaysInMonth = (month: number) => new Date(new Date().getFullYear(), month, 0).getDate();
+
+const getBranchDayOfYear = (name: string) => {
+    const tokens = name.split('/');
+    const monthAndDay = tokens[tokens.length - 1].split('.')[0];
+    if (monthAndDay.length === 3) {
+        const month = parseInt(monthAndDay[0], 10);
+        const day = parseInt(monthAndDay.slice(1, 3), 10);
+        return getDaysInMonth(month) * month + day;
+    } else {
+        const month = parseInt(monthAndDay.slice(0, 2), 10);
+        const day = parseInt(monthAndDay.slice(2, 4), 10);
+        return getDaysInMonth(month) * month + day;
+    }
+};
+
+const getAllBranchesRelease = () => new Promise<string[]>((resolve, reject) => {
+    const command = 'git branch -a | egrep \'remotes/origin/[[:digit:]]{3,4}\\.[[:digit:]]{3,4}\'';
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            reject(error.message);
+            return;
+        }
+        if (stderr) {
+            reject(stderr);
+            return;
+        }
+        resolve(stdout.split('\n').filter(b => b).map(b => b.trim().split('/')[2]));
+    });
+});
+
+(async () => {
+    const branchNames: string[] = await getAllBranchesRelease();
+    const current = getCurrentDayOfYear();
+
+    const outdatedBranches = branchNames.filter(branchName => current - getBranchDayOfYear(branchName) > 7);
+
+    const commands = outdatedBranches.map(branchName =>
+        `git push origin --delete ${branchName} || echo branch '${branchName}' does not exist`);
+
+    commands.forEach(command => {
+        exec(command, (error, stdout, stderr) => {
+            console.log(`Executing: ${command}`);
+
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+        });
+    });
+})();
